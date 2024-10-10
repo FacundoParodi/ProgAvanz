4fdc86c5e5a25f784ea0c2b0b03db4bf438fe995
FacundoParodi <127805192+FacundoParodi@users.noreply.github.com>
2024-10-10 02:06:26 -0300

    Delete TP1_Funciones directory

--- TP1_Funciones/TP1_Vectores/ej10.js
+++
@@ -1,9 +0,0 @@
-/*Uso de sort:
-Crea un array de palabras y ordénalo alfabéticamente usando sort.*/
-
-
-let palabras= ["termo", "mate", "chicles", "monitor", "billetera"]
-
-
-palabras.sort()
-console.log(palabras)
--- TP1_Funciones/TP1_Vectores/ej3.js
+++
@@ -1,9 +0,0 @@
-/*Iterar sobre un Array:
-○ Usa un bucle for para iterar sobre el array frutas e imprimir cada
-elemento.*/
-
-let frutas= ["manzana", "banana", "pera"];
-
-for (let i=0; i < frutas.length; i++) {
-    console.log(frutas[i]);
-}
